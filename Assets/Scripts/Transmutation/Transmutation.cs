44689e531147d203ebb1ac1d2fa1db74f7cfa618
Cmanchik <aksenova1648@gmail.com>
2021-05-28 13:20:53 +0300

    Смена брони одной на другую

---
+++ Assets/Scripts/Transmutation/Transmutation.cs
@@ -0,0 +1,22 @@
+﻿using CharacteristicsPersons;
+using Transmutation.ArmorSets;
+using UnityEngine;
+
+namespace Transmutation
+{
+    [RequireComponent(typeof(Characteristics))]
+    public class Transmutation : MonoBehaviour
+    {
+        
+        private Characteristics _characteristics;
+        private void Start()
+        {
+            _characteristics = GetComponent<Characteristics>();
+        }
+
+        public void ChangeArmor(ArmorSet armor)
+        {
+            armor.Init(_characteristics);
+        }
+    }
+}
